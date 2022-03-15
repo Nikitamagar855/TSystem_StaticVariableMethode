137d65c8c6985cbb79b6d76994a3d195b4765ace
Nikitamagar855 <89832430+Nikitamagar855@users.noreply.github.com>
2022-03-15 17:17:47 +0530

    Add project files.

---
+++ TSystem_StaticVariableMethode/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystem_StaticVariableMethode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee( "nikita","nashik");
+            Console.WriteLine( (emp.GetData()));
+            Console.WriteLine(emp.Get);
+            Console.ReadLine();
+        }
+    }
+}
